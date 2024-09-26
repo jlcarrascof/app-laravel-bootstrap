956871f22e4f532aa11f721bde2a29fd3b54fbdd
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-25 20:50:16 -0400

    Refactor home page to add navigation menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,16 @@
     <title>Home</title>
 </head>
 <body>
+
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/portfolio">Portfolio</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+    </nav>
+
     <h1>Welcome to Home Page</h1>
 
     Welcome <?php echo $name ?? "Guest"; ?> <br>
