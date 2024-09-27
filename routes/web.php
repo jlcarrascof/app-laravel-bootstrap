50429d79927933fa42eb9380a4e22384966ab31e
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-27 19:01:02 -0400

    Refactor web.php to pass portfolio data to the portfolio view

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,5 @@ $portfolio = [
 
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
-Route::view('/portfolio', 'portfolio')->name('portfolio');
+Route::view('/portfolio', 'portfolio', compact('portfolio'))->name('portfolio');
 Route::view('/contact', 'contact')->name('contact');
