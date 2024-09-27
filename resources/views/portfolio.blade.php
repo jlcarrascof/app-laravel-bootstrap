05e36ee159339a2000ed9ac323dfd5e69bda8979
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-27 19:10:58 -0400

    Refactor portfolio view to use Blade templating syntax for displaying portfolio items

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -6,10 +6,8 @@
     <h1>Portfolio</h1>
 
     <ul>
-
-        <?php foreach ($portfolio as $portfolioItem) {
-            echo '<li>'.$portfolioItem['title'].'</li>';
-            }
-        ?>
+        <?php foreach ($portfolio as $portfolioItem): ?>
+            <li>{{ $portfolioItem['title'] }}</li>
+        <?php endforeach; ?>
     </ul>
 @endsection
