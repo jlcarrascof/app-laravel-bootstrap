141de75bad186b9e2a34b76672d0c0e8c69c1006
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-27 20:44:19 -0400

    Refactor web.php to remove commented out portfolio items

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 // Route::view('/', 'home')->name('home'); // Privacy Policy Page, Terms of Service Page, etc.
 
+
 $portfolio = [
-    ['title' => 'Project #1'],
+/*    ['title' => 'Project #1'],
     ['title' => 'Project #2'],
     ['title' => 'Project #3'],
-    ['title' => 'Project #4'],
+    ['title' => 'Project #4'], */
 ];
 
 Route::view('/', 'home')->name('home');
