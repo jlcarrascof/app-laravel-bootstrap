7448ba994c1786b15e04fcea3ace5365bb5b3960
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-27 20:55:34 -0400

    Refactor portfolio view to conditionally display portfolio items

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -10,6 +10,8 @@
             @foreach ($portfolio as $portfolioItem)
                 <li>{{ $portfolioItem['title'] }}</li>
             @endforeach
+        @else
+            <li>No portfolio items found</li>
         @endif
     </ul>
 @endsection
