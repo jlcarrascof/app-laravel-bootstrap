ca4be0fd30add4fd9cba0a66264ea403861282ed
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-07 11:54:17 -0400

    Refactor route for portfolio to use GET method and update PortfolioController

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -7,21 +7,78 @@ use Illuminate\Http\Request;
 class PortfolioController extends Controller
 {
     /**
-     * Handle the incoming request.
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function store(Request $request)
     {
+        //
+    }
 
-        $portfolio = [
-            ['title' => 'Project #1'],
-            ['title' => 'Project #2'],
-            ['title' => 'Project #3'],
-            ['title' => 'Project #4'],
-        ];
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
 
-        return view('portfolio', compact('portfolio'));
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,4 @@ use App\Http\Controllers\PortfolioController;
 
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
-Route::get('/portfolio', PortfolioController::class)->name('portfolio');
+// Route::get('/portfolio', PortfolioController::class)->name('portfolio');
