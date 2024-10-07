3d6f0145b2d0c7659e22bea45461da4215c279aa
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-07 10:58:46 -0400

    Refactor PortfolioController to add request handling and update portfolio view

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 // Route::view('/', 'home')->name('home'); // Privacy Policy Page, Terms of Service Page, etc.
 
-
-$portfolio = [
-    ['title' => 'Project #1'],
-    ['title' => 'Project #2'],
-    ['title' => 'Project #3'],
-    ['title' => 'Project #4'],
-];
-
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
-Route::view('/portfolio', 'portfolio', compact('portfolio'))->name('portfolio');
 Route::view('/portfolio', 'PortfolioController');
