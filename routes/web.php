a0a5df61456496c2b5e004d39796997bcd27a768
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-07 12:06:27 -0400

    Refactor route for portfolio to use resourceful routing in PortfolioController

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,4 @@ use App\Http\Controllers\PortfolioController;
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
 // Route::get('/portfolio', PortfolioController::class)->name('portfolio');
+Route::resource('/portfolio', PortfolioController::class);
