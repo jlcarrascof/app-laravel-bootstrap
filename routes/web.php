f3fa473e183d392ecc451c6bd3554a549b1fd82f
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-07 11:37:37 -0400

    Refactor route for portfolio to use GET method and update PortfolioController

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PortfolioController;
 
 // Route::view('/', 'home')->name('home'); // Privacy Policy Page, Terms of Service Page, etc.
 
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
-Route::get('/portfolio', 'PortfolioController');
+Route::get('/portfolio', PortfolioController::class)->name('portfolio');
