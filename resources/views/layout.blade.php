80ab6f1ebd6558095ad0b10401963d0bc5ebd34e
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-10 23:05:31 -0400

    Refactor layout.blade.php to extract navigation into a partial view

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,14 +10,7 @@
     </style>
 </head>
 <body>
-    <nav>
-        <ul>
-            <li class="{{ setActive('home') }}"><a href="/">Home</a></li>
-            <li class="{{ setActive('about') }}"><a href="/about">About</a></li>
-            <li class="{{ setActive('portfolio') }}"><a href="/portfolio">Portfolio</a></li>
-            <li class="{{ setActive('contact') }}"><a href="/contact">Contact</a></li>
-        </ul>
-    </nav>
+
     @yield('content')
 </body>
 </html>
