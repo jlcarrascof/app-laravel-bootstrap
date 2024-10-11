f3ea764c3e7a6e279f6de11ea8486f218583a4a1
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-10 21:06:47 -0400

    Refactor layout.blade.php to add active class to the Home link

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,7 +12,7 @@
 <body>
     <nav>
         <ul>
-            <li class="active"><a href="/">Home</a></li>
+            <li class="{{ request()->routeIs('home') ? 'active' : ''}}"><a href="/">Home</a></li>
             <li><a href="/about">About</a></li>
             <li><a href="/portfolio">Portfolio</a></li>
             <li><a href="/contact">Contact</a></li>
