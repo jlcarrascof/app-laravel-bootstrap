06832f562367b293d97e03e244044b429eb0f2a7
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-10 21:37:13 -0400

    Refactor layout.blade.php to update active class in navigation links

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,10 +12,10 @@
 <body>
     <nav>
         <ul>
-            <li class="{{ request()->routeIs('home') ? 'active' : ''}}"><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/portfolio">Portfolio</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <!--<li class="{{ setActive('home') }}"><a href="/">Home</a></li> -->
+            <li class="{{ request()->routeIs('about') ? 'active' : ''}}"><a href="/about">About</a></li>
+            <li class="{{ request()->routeIs('portfolio') ? 'active' : ''}}"><a href="/portfolio">Portfolio</a></li>
+            <li class="{{ request()->routeIs('contact') ? 'active' : ''}}"><a href="/contact">Contact</a></li>
         </ul>
     </nav>
     @yield('content')
