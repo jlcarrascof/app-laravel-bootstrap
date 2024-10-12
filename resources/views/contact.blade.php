891df97d85aed548dedc139754e6c069f6cbc131
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-12 19:46:42 -0400

    Refactor contact.blade.php to add a contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,4 +4,13 @@
 
 @section('content')
     <h1>Contact</h1>
+
+    <form>
+        <input name="name" placeholder="Name... ">
+        <input type="email" name="email" placeholder="Email... ">
+        <input name="subject" placeholder="Subject... ">
+        <textarea name="content" placeholder="Message... "></textarea>
+
+        <button >Send</button>
+    </form>
 @endsection
