f768c8cfc64066db5ac43acf3998a021a1e38aaa
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-12 20:58:43 -0400

    Refactor contact form to include default values

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,10 +7,10 @@
 
     <form method="POST" action="{{ route('contact') }}">
         @csrf
-        <input name="name" placeholder="Name... "><br>
-        <input type="email" name="email" placeholder="Email... "><br>
-        <input name="subject" placeholder="Subject... "><br>
-        <textarea name="content" placeholder="Message... "></textarea><br>
+        <input name="name" placeholder="Name... " value="Javier"><br>
+        <input type="email" name="email" placeholder="Email... " value="javier@javier.com"><br>
+        <input name="subject" placeholder="Subject... " value="Testing subject"><br>
+        <textarea name="content" placeholder="Message... ">Testing Message</textarea><br>
         <button >Send</button>
     </form>
 @endsection
