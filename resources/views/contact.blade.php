291cc09504518fe7961bad3cc66dacf8b30122a1
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-13 11:54:47 -0400

    Refactor contact form to include error message display for name and email fields

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,17 +4,12 @@
 
 @section('content')
     <h1>Contact</h1>
-    @if ($errors->any())
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
     <form method="POST" action="{{ route('contact') }}">
         @csrf
         <input name="name" placeholder="Name... " value=""><br>
+        {{ $errors->first('name') }}<br>
         <input type="email" name="email" placeholder="Email... " value=""><br>
+        {{ $errors->first('email') }}<br>
         <input name="subject" placeholder="Subject... " value="Testing subject"><br>
         <textarea name="content" placeholder="Message... ">Testing Message</textarea><br>
         <button >Send</button>
