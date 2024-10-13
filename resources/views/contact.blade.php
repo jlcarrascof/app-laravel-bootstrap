23a10ecbecb765eec9b09b95108c68722e20a43f
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-13 12:36:16 -0400

    Refactor contact form to include error message display in a list and use HTML entities for error message display

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,9 +7,9 @@
     <form method="POST" action="{{ route('contact') }}">
         @csrf
         <input name="name" placeholder="Name... " value=""><br>
-        {!! $errors->first('name', '<small>:message</small>') !!}<br>
+        {!! $errors->first('name', '<small>:message</small><br>') !!}
         <input type="email" name="email" placeholder="Email... " value=""><br>
-        {!! $errors->first('email','<small>:message</small>') !!}<br>
+        {!! $errors->first('email','<small>:message</small><br>') !!}
         <input name="subject" placeholder="Subject... " value="Testing subject"><br>
         <textarea name="content" placeholder="Message... ">Testing Message</textarea><br>
         <button >Send</button>
