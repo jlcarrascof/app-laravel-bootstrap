a928ab19444f8c3c0139a73a090705f52b6dcc95
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-13 11:02:31 -0400

    Refactor nav.blade.php to fix formatting of the Portfolio link

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -2,7 +2,9 @@
     <ul>
         <li class="{{ setActive('home') }}"><a href="/">Home</a></li>
         <li class="{{ setActive('about') }}"><a href="/about">About</a></li>
-        <li class="{{ setActive('portfolio') }}"><a href="/portfolio">Portfolio</a></li>
+        <li class="{{ setActive('portfolio') }}">
+            <a href="/portfolio">Portfolio</a>
+        </li>
         <li class="{{ setActive('contact') }}"><a href="/contact">Contact</a></li>
     </ul>
 </nav>
